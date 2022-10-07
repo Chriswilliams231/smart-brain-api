83798d8f134feb7070f635610cddf7b077bd0198
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-10-06 18:59:53 -0700

    early stages of connecting the server to a database

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const pg = require('knex')
 
+const db = pg({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'christopherwilliams',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+db.select('*').from('users').then(data => {
+    console.log(data)
+});
 const app = express()
 
 app.use(bodyParser.json());
@@ -56,14 +70,17 @@ app.post('/signin', (req, res) => {
 // Register endpoint
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length - 1])
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0])
+        })
+        .catch(err => res.status(400).json("unable to register"))
 })
 
 
